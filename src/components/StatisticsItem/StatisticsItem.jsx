afa20d849733f7d0d9a072b6a60e479b4db3b7b0
Anastasiia Skliarovska <skliarovskaa@gmail.com>
2025-02-22 16:56:57 +0200

    did tasks

--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -1,5 +1,17 @@
-const StatisticsItem = () => {
-  return <h3>StatisticsItem</h3>;
+import { IconContext } from 'react-icons';
+import style from './StatisticsItem.module.css';
+
+const StatisticsItem = ({ data: { title, total }, icon }) => {
+  return (
+    <>
+      <IconContext.Provider value={{ color: '#2196f3', size: '24px' }}>
+        {icon}
+      </IconContext.Provider>
+
+      <span className={style.counter}>{total}</span>
+      <p className={style.text}>{title}</p>
+    </>
+  );
 };
 
 export default StatisticsItem;
